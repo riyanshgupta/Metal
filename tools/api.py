ba04c9ce0b4b4f48b41397e46b23a9e8ab952b62
riyanshgupta <riyanshgupta794@gmail.com>
2024-03-08 01:18:03 +0530

    some

---
+++ tools/api.py
@@ -0,0 +1,66 @@
+import requests, json
+# from tools import data
+from requests_html import HTMLSession
+from bs4 import BeautifulSoup
+
+def sec(time_str:str):
+    parts = time_str.split(":")
+    hours = 0
+    minutes = 0
+    seconds = 0
+    if len(parts) == 2:
+        minutes = int(parts[0])
+        seconds = int(parts[1])
+    elif len(parts) == 1:
+        seconds = int(parts[0])
+    return hours * 3600 + minutes * 60 + seconds
+
+def video(name:str):
+    url = "https://www.youtube.com/results"
+    payloads = { "search_query":name+" @puregym" }
+    headers = { "User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36" }
+    session = HTMLSession()
+    response = session.get(url=url, headers=headers, params=payloads)
+    html_content = response.html.html
+    session.close()
+    soup = BeautifulSoup(html_content,'lxml')
+    script_tag = soup.find('script', text=lambda text: text and 'var ytInitialData' in text)
+    script_content = script_tag.string
+    start_index = script_content.find('var ytInitialData = ') + len('var ytInitialData = ')
+    end_index = script_content.find('};', start_index) + 1
+    yt_initial_data = script_content[start_index:end_index]
+    data = json.loads(yt_initial_data)
+    l = []
+    for i in data.get("contents").get("twoColumnSearchResultsRenderer").get("primaryContents").get("sectionListRenderer").get("contents")[0].get("itemSectionRenderer").get("contents"):
+        if i.get("videoRenderer")!=None:
+            if sec(i["videoRenderer"]["lengthText"]["simpleText"]) <= 29:
+                return {
+                    "id": i["videoRenderer"]["videoId"], 
+                    "title": i["videoRenderer"]["title"]["runs"][0]["text"], 
+                    "thumbnail": i["videoRenderer"]["thumbnail"][ "thumbnails"][0]["url"]
+                }   
+    return None
+
+def get_exercise(muscle:str, category:str):
+# category is the equipments and muscles are muscles I mean that's readable
+    url = f"https://musclewiki.com/newapi/exercise/exercises/?limit=20&muscles={muscle}&category={category}"
+    headers = {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36"}
+    response = requests.request(method="GET", url=url, headers=headers)
+    
+    with open(file="data.json", mode="w", encoding="utf-8") as f:
+        f.write(response.text)
+        
+    res = response.json()
+    result = []
+    for i in res.get("results"):
+        if i.get("name")!=None:
+            result.append({
+                "id": i["id"],
+                "name": i["name"],
+                "difficulty": i.get("difficulty"),
+                "correct_steps": i.get("correct_steps"),
+                "muscles": i.get("muscles"),
+                "url": "/exercise/"+((i["name"]).replace(' ', '-')).lower()
+        })
+    
+    return result
---
+++ tools/data.py
@@ -0,0 +1,209 @@
+muscle_groups = {
+    "Biceps": [
+        {"id": 1, "name": "Biceps"},
+        {"id": 16, "name": "Long Head Bicep"},
+        {"id": 17, "name": "Short Head Bicep"}
+    ],
+    "Traps": [
+        {"id": 14, "name": "Traps (mid-back)"},
+        {"id": 4, "name": "Traps"},
+        {"id": 41, "name": "Upper Traps"},
+        {"id": 42, "name": "Lower Traps"}
+    ],
+    "Lower back": [
+        {"id": 13, "name": "Lower back"}
+    ],
+    "Abdominals": [
+        {"id": 12, "name": "Abdominals"},
+        {"id": 21, "name": "Lower Abdominals"},
+        {"id": 22, "name": "Upper Abdominals"}
+    ],
+    "Calves": [
+        {"id": 11, "name": "Calves"},
+        {"id": 31, "name": "Tibialis"},
+        {"id": 32, "name": "Soleus"},
+        {"id": 33, "name": "Gastrocnemius"}
+    ],
+    "Forearms": [
+        {"id": 10, "name": "Forearms"},
+        {"id": 25, "name": "Wrist Extensors"},
+        {"id": 26, "name": "Wrist Flexors"}
+    ],
+    "Glutes": [
+        {"id": 9, "name": "Glutes"},
+        {"id": 37, "name": "Gluteus Medius"},
+        {"id": 38, "name": "Gluteus Maximus"}
+    ],
+    "Hamstrings": [
+        {"id": 8, "name": "Hamstrings"},
+        {"id": 39, "name": "Medial Hamstrings"},
+        {"id": 40, "name": "Lateral Hamstrings"}
+    ],
+    "Lats": [
+        {"id": 7, "name": "Lats"}
+    ],
+    "Shoulders": [
+        {"id": 6, "name": "Shoulders"},
+        {"id": 18, "name": "Lateral Deltoid"},
+        {"id": 19, "name": "Anterior Deltoid"},
+        {"id": 20, "name": "Posterior Deltoid"}
+    ],
+    "Triceps": [
+        {"id": 5, "name": "Triceps"},
+        {"id": 34, "name": "Long Head Tricep"},
+        {"id": 35, "name": "Lateral Head Triceps"},
+        {"id": 36, "name": "Medial Head Triceps"}
+    ],
+    "Quads": [
+        {"id": 3, "name": "Quads"},
+        {"id": 27, "name": "Inner Thigh"},
+        {"id": 28, "name": "Inner Quadriceps"},
+        {"id": 29, "name": "Outer Quadricep"},
+        {"id": 30, "name": "Rectus Femoris"}
+    ],
+    "Chest": [
+        {"id": 2, "name": "Chest"},
+        {"id": 23, "name": "Upper Pectoralis"},
+        {"id": 24, "name": "Mid and Lower Chest"}
+    ],
+    "Obliques": [
+        {"id": 15, "name": "Obliques"}
+    ],
+    "Hands": [
+        {"id": 43, "name": "Hands"}
+    ],
+    "Feet": [
+        {"id": 46, "name": "Feet"}
+    ],
+    "Front Shoulders": [
+        {"id": 47, "name": "Front Shoulders"}
+    ],
+    "Rear Shoulders": [
+        {"id": 48, "name": "Rear Shoulders"}
+    ]
+}
+muscles = {
+    "Biceps": {
+        "id": 1, 
+        "name": "Biceps"
+    },
+    "Traps": {
+        "id": 4, 
+        "name": "Traps"
+    },
+    "Lower back": {
+        "id": 13, 
+        "name": "Lower back"
+    },
+    "Abdominals": {
+        "id": 12, 
+        "name": "Abdominals"
+    },
+    "Calves": {
+        "id": 11, 
+        "name": "Calves"
+    },
+    "Forearms": {
+        "id": 10, 
+        "name": "Forearms"
+    },
+    "Glutes": {
+        "id": 9, 
+        "name": "Glutes"
+    },
+    "Hamstrings": {
+        "id": 8, 
+        "name": "Hamstrings"
+    },
+    "Lats": {
+        "id": 7, 
+        "name": "Lats"
+    },
+    "Shoulders": {
+        "id": 6, 
+        "name": "Shoulders"
+    },
+    "Triceps": {
+        "id": 5, 
+        "name": "Triceps"
+    },
+    "Quads": {
+        "id": 3, 
+        "name": "Quads"
+    },
+    "Chest": {
+        "id": 2, 
+        "name": "Chest"
+    },
+    "Obliques": {
+        "id": 15, 
+        "name": "Obliques"
+    }
+}
+
+equipments = {
+        "Barbell":{
+            "id": 1,
+            "name": "Barbell"
+        },
+        "Dumbbells":{
+            "id": 2,
+            "name": "Dumbbells"
+        },
+        "Bodyweight": {
+            "id": 3,
+            "name": "Bodyweight"
+        },
+        "Machine": {
+            "id": 4,
+            "name": "Machine"
+        },
+        "Medicine-Ball": {
+            "id": 6,
+            "name": "Medicine-Ball"
+        },
+        "Kettlebells": {
+            "id": 7,
+            "name": "Kettlebells"
+        },
+        "Stretches": {
+            "id": 8,
+            "name": "Stretches"
+        },
+        "Cables": {
+            "id": 9,
+            "name": "Cables"
+        },
+        "Band": {
+            "id": 10,
+            "name": "Band"
+        },
+        "Plate": {
+            "id": 11,
+            "name": "Plate"
+        },
+        "TRX": {
+            "id": 12,
+            "name": "TRX"
+        },
+        "Yoga": {
+            "id": 13,
+            "name": "Yoga"
+        },
+        "Bosu": {
+            "id": 16,
+            "name": "Bosu"
+        },
+        "Bosu-Ball": {
+            "id": 24,
+            "name": "Bosu-Ball"
+        },
+        "Cardio": {
+            "id": 27,
+            "name": "Cardio"
+        },
+        "Smith-Machine":{
+            "id": 85,
+            "name": "Smith-Machine"
+        }
+}
