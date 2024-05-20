30ee981d54f1c6e40f0cf0cb8a6c33bd82bbfc80
Riyansh Gupta <100010032+riyanshgupta@users.noreply.github.com>
2024-05-20 18:22:38 +0530

    Update api.py

--- tools/api.py
+++ tools/api.py
@@ -43,7 +43,7 @@ def video(name:str):
 
 def get_exercise(muscle:str, category:str):
 # category is the equipments and muscles are muscles I mean that's readable
-    url = f"https://musclewiki.com/newapi/exercise/exercises/?limit=20&muscles={muscle}&category={category}"
+    url = f"link"
     headers = {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36"}
     response = requests.request(method="GET", url=url, headers=headers)
         
